468a5fc9a3f1e6a8a637b93490821e51487b445f
Pavel Krymets <pavel@krymets.com>
2014-03-25 16:56:07 +0200

    adb log support

--- profiler.ps1
+++ profiler.ps1
@@ -6,9 +6,41 @@ foreach ($assembly in $assemblies) { [System.Reflection.Assembly]::LoadFrom($ass
 }
  
 #Load-Packages
- 
+$global:job = $null
+
 $routes = @{
     "/" = { return [System.IO.File]::ReadAllText("index.html") }
+    "/start" = {
+        Write-Host "Starting job"
+        $global:job = Start-Job -scriptblock {
+            $logName = "z:\log.txt"
+            if (Test-Path $logName)
+            {
+                Remove-Item $logName
+            }
+
+            adb logcat
+        }
+        Receive-Job -Job $global:job;
+        return 'OK';
+    }
+    "/log" = { 
+        if ($global:job -eq $null)
+        {
+            return "NO JOB"
+        }
+        return (Receive-Job -Job $global:job) -join "`r`n" | Out-String
+    }
+    "/stop" = {
+
+        if ($global:job -eq $null)
+        {
+            return "NO JOB"
+        }
+        Stop-Job -Job $global:job
+        $global:job = $null
+        return 'OK';
+    }
 }
  
 $url = 'http://localhost:12333/'
@@ -16,35 +48,38 @@ $listener = New-Object System.Net.HttpListener
 $listener.Prefixes.Add($url)
 $listener.Start()
  
+Start-Process -FilePath  $url
 Write-Host "Listening at $url..."
 
-Start-Process -FilePath  $url
+
 
 while ($listener.IsListening)
 {
     $context = $listener.GetContext()
-    $requestUrl = $context.Request.Url
-    $response = $context.Response
+    
+        $requestUrl = $context.Request.Url
+        $response = $context.Response
      
-    Write-Host ''
-    Write-Host "> $requestUrl"
+        Write-Host ''
+        Write-Host "> $requestUrl"
      
-    $localPath = $requestUrl.LocalPath
-    $route = $routes.Get_Item($requestUrl.LocalPath)
+        $localPath = $requestUrl.LocalPath
+        $route = $routes.Get_Item($requestUrl.LocalPath)
      
-    if ($route -eq $null)
-    {
-        $response.StatusCode = 404
-    }
-    else
-    {
-        $content = & $route
-        $buffer = [System.Text.Encoding]::UTF8.GetBytes($content)
-        $response.ContentLength64 = $buffer.Length
-        $response.OutputStream.Write($buffer, 0, $buffer.Length)
-    }
-    $response.Close()
+        if ($route -eq $null)
+        {
+            $response.StatusCode = 404
+        }
+        else
+        {
+            $content = & $route
+            $buffer = [System.Text.Encoding]::UTF8.GetBytes($content)
+            $response.ContentLength64 = $buffer.Length
+            $response.OutputStream.Write($buffer, 0, $buffer.Length)
+        }
+        $response.Close()
      
-    $responseStatus = $response.StatusCode
-    Write-Host "< $responseStatus"
+        $responseStatus = $response.StatusCode
+        Write-Host "< $responseStatus"
+    
 }
